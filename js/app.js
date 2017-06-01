4432865c90f4cd8139e4433c8524a3fcff3eb75b
Nicole Aliste <nicole.alistecastillo@gmail.com>
2017-06-01 01:29:42 -0400

    se incorpora work al proyecto, hay una falla con el footer

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
 /*
  * Archivo principal de funcionalidad de JS
  */
+ (function main(){
+	var boxes = Array.from(document.getElementsByClassName("img"));
+	var work = document.getElementById("work");
+
+	var modal, bodyModal, close, img;
+	boxes.forEach(function(box){
+		box.addEventListener("click", function(){
+			modal = document.createElement("div");
+			modal.classList.add("box-services-modal");
+			bodyModal = document.createElement("div");
+			bodyModal.classList.add("modal-body");
+			bodyModal.innerHTML = box.innerHTML;			
+			modal.appendChild(bodyModal);
+			
+			close = document.createElement("div");
+			close.classList.add("close");
+			img = document.createElement("img");
+			img.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
+			close.appendChild(img);
+			modal.appendChild(close);
+			work.appendChild(modal);
+			close.addEventListener("click",function(){
+				modal.classList.add("hide");
+				work.removeChild(modal);
+			});
+		});		
+	});
+})();
